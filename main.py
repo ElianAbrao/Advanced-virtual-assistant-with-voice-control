04513cf75691b686bd24186ccc40b3014f14df65
Elian Abrão <88802059+ElianAbrao@users.noreply.github.com>
2021-09-16 15:37:49 -0300

    Update main.py
    Updating the main function, changing the speech recognition mode

--- main.py
+++ main.py
@@ -1,90 +1,33 @@
-#!/usr/bin/env python3
-
-import argparse
+#Importing Libraries
+import speech_recognition as sr
 import os
-import queue
-import sounddevice as sd
-import vosk
-import sys
 
-q = queue.Queue()
+#Main Function
+def voice_recorder():
+#enable user microphone
+    recorder = sr.Recognizer()
 
-def int_or_str(text):
-    """Helper function for argument parsing."""
+#using microphone recordings
+    with sr.Microphone() as source:
+        recorder.adjust_for_ambient_noise(source)
+        audio = recorder.listen(source)
+        
     try:
-        return int(text)
-    except ValueError:
-        return text
+        speech = recorder.recognize_google(audio, language='pt-BR')
 
-def callback(indata, frames, time, status):
-    """This is called (from a separate thread) for each audio block."""
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
+        if ('Abra' in speech and ('ópera' in speech) or 'Google' in speech):
+            os.system("start opera.exe")
+        
+        if 'Abra' in speech and 'calculadora' in speech:
+            os.system('start calc.exe')
 
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument(
-    '-l', '--list-devices', action='store_true',
-    help='show list of audio devices and exit')
-args, remaining = parser.parse_known_args()
-if args.list_devices:
-    print(sd.query_devices())
-    parser.exit(0)
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    parents=[parser])
-parser.add_argument(
-    '-f', '--filename', type=str, metavar='FILENAME',
-    help='audio file to store recording to')
-parser.add_argument(
-    '-m', '--model', type=str, metavar='MODEL_PATH',
-    help='Path to the model')
-parser.add_argument(
-    '-d', '--device', type=int_or_str,
-    help='input device (numeric ID or substring)')
-parser.add_argument(
-    '-r', '--samplerate', type=int, help='sampling rate')
-args = parser.parse_args(remaining)
+#returns the pronounced sentence
+        print('Você disse: '+ speech)
+    
+    except sr.UnknownValueError:
+#Indicates that the speech recognizer did not understand what was said
+        print("Desculpe eu não entendi oque você disse!")
+    
+    return speech
 
-try:
-    if args.model is None:
-        args.model = "model"
-    if not os.path.exists(args.model):
-        print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-        print ("and unpack as 'model' in the current folder.")
-        parser.exit(0)
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, 'input')
-        # soundfile expects an int, sounddevice provides a float:
-        args.samplerate = int(device_info['default_samplerate'])
-
-    model = vosk.Model(args.model)
-
-    if args.filename:
-        dump_fn = open(args.filename, "wb")
-    else:
-        dump_fn = None
-
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
-                            channels=1, callback=callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
-
-            rec = vosk.KaldiRecognizer(model, args.samplerate)
-            while True:
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    print(rec.Result())
-                else:
-                    print(rec.PartialResult())
-                if dump_fn is not None:
-                    dump_fn.write(data)
-
-except KeyboardInterrupt:
-    print('\nDone')
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
-
+voice_recorder()
